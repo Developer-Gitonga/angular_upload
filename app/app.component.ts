7e4f0ee0ff82dc0248852cd58ff7bfa183ecf02c
Develper-Gitonga <aizensalim52@gmail.com>
2022-07-11 15:40:51 +0300

    ch:boilerplate series

---
+++ app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component, VERSION } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  url = 'https://img.icons8.com/ios/100/000000/contract-job.png';
+  onSelect(event) {
+    let fileType = event.target.files[0].type;
+    if (fileType.match(/image\/*/)) {
+      let reader = new FileReader();
+      reader.readAsDataURL(event.target.files[0]);
+      reader.onload = (event: any) => {
+        this.url = event.target.result;
+      };
+    } else {
+      window.alert('Please select correct image format');
+    }
+  }
+}
